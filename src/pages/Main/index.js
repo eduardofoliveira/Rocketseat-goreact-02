bbb79324212e563fb295336fb8b1e8fa953cbb19
eduardofoliveira <eduardo_felipe_oliveira@yahoo.com.br>
2019-03-02 22:50:45 -0300

    Modulo pronto

---
+++ src/pages/Main/index.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+import api from '../../services/api';
+
+import logo from '../../assets/logo.png';
+
+import { Container, Form } from './styles';
+
+import CompareList from '../../components/CompareList';
+
+export default class Main extends Component {
+  state = {
+    loading: false,
+    repositoryError: false,
+    repositoryInput: '',
+    repositories: [],
+  };
+
+  handleAddRepository = async (e) => {
+    e.preventDefault();
+    this.setState({ loading: true });
+
+    try {
+      const { repositoryInput, repositories } = this.state;
+      const { data: repository } = await api.get(`/repos/${repositoryInput}`);
+
+      repository.lastCommit = moment(repository.pushed_at).fromNow();
+
+      this.setState({
+        repositoryInput: '',
+        repositories: [...repositories, repository],
+        repositoryError: false,
+      });
+    } catch (error) {
+      this.setState({ repositoryError: true });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  render() {
+    const {
+      repositoryError, repositoryInput, loading, repositories,
+    } = this.state;
+
+    return (
+      <Container>
+        <img src={logo} alt="Github Compare" />
+
+        <Form withError={repositoryError} onSubmit={this.handleAddRepository}>
+          <input
+            type="text"
+            placeholder="usuário/repositorio"
+            value={repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">{loading ? <i className="fa fa-spinner fa-pulse" /> : 'OK'}</button>
+        </Form>
+
+        <CompareList repositories={repositories} />
+      </Container>
+    );
+  }
+}
